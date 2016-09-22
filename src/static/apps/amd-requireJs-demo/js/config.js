05dac57de27e7b50a21bd1c224f5d5234e77214d
franks2016 <279811056@qq.com>
2016-09-22 10:22:45 +0800

    amd演示下系统模快配置文件优化

--- src/static/apps/amd-requireJs-demo/js/config.js
+++ src/static/apps/amd-requireJs-demo/js/config.js
@@ -21,11 +21,11 @@ require.config({
     urlArgs: "bust=" +  (new Date()).getTime()
 });
 /* 完整化paths与shim */
-
+(function(){
     var sysAlias = requirejs.s.contexts._.config.paths;
     var sysShim = requirejs.s.contexts._.config.shim;
     requirejs.s.contexts._.config.paths = newAlias.aliasIntegrate(sysAlias,'amd');
     requirejs.s.contexts._.config.shim = newAlias.shimIntegrate(sysShim,'amd');
-
+})();
 /*console.log(requirejs.s.contexts._.config.paths);
 console.log(requirejs.s.contexts._.config.shim);*/
